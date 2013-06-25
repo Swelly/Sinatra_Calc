5d150b5efe2a619f73845447e609f4ff4984bd00
Wesley Carr <surfswell17@gmail.com>
2013-06-25 11:33:00 -0400

    Tested urls by defining parameters

--- main.rb
+++ main.rb
@@ -22,3 +22,38 @@ get '/ahoy_you/:name' do
   @name = params[:name]
   return "Ahoy, #{@name}"
 end
+
+#localhost:4567/names/wesley/carr
+get '/names/:first/:last' do
+  @first = params[:first]
+  @last = params[:last]
+  return "You can do multiple parameters,
+          #{@first.capitalize} #{@last.capitalize}!"
+end
+
+#localhost:4567/calc/add/1/2
+get '/calc/add/:first/:second' do
+  @first = params[:first].to_i
+  @second = params[:second].to_i
+  return (@first + @second).to_s
+end
+
+get '/calc/subtract/:first/:second' do
+  @first = params[:first].to_i
+  @second = params[:second].to_i
+  return (@first - @second).to_s
+end
+
+#Returns the product of two numbers
+# localhost:4567/calc/multiply/5/10
+get '/calc/multiply/:first/:second' do
+  @first = params[:first].to_i
+  @second = params[:second].to_i
+  return (@first * @second).to_s
+end
+
+get '/calc/divide/:first/:second' do
+  @first = params[:first].to_i
+  @second = params[:second].to_i
+  return (@first / @second).to_s
+end
